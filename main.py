d06ed54ed9117ca186bb8eaf67e5a70f66acc7a3
Apex <dr.gorodnov@gmail.com>
2024-04-17 21:22:13 -0700

    Update main.py
    It's working just not a command

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-_Token = str(open("./GPTToken","r").readline())
-print(_Token)
+from openai import OpenAI
+import json
+
+_Token = str(open("./GPTToken", "r").readline())
+client = OpenAI(api_key=_Token)
+
+
+def chat_with_gpt(prompt):
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=[{"role": "user", "content": prompt}],
+    )
+    return json.loads(response.model_dump_json())["choices"][0]["message"]["content"]
+
+
+while True:
+    prompt = input()
+    if prompt == "exit":
+        exit()
+    response = chat_with_gpt(prompt)
+    print(response)
